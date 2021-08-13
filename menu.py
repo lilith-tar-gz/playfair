7dc46136897301504c5b348bb2906b1acd93e739
lilith.tar.gz <62079500+lilith-tar-gz@users.noreply.github.com>
2021-08-13 14:17:23 +0300

    menu ready

--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+from decrypt_playfair import decryption
+from encrypt_playfair import encryption
+
 print('Welcome to our playfair calculator!')
 print("Let's create our array! Think of a keyword. Enter the letters of the word in UPPERCASE without repeating them. Fill the rest of the array with the letters of the alphabet alphabetically skipping J and the letters you've already used ")
 key_table = [[0 for i in range(5)] for j in range(5)] 
@@ -8,11 +11,14 @@ print("Okay now that our array is ready it's time to decide what you want to do 
 while True:
     option = input('Pick something! For encryption enter A for decryption enter B : ')
     if option != 'A' and option != 'B':
-        print("Sorry, I have no idea what to do with that:( Can you give me something that looks like these 2? (A or B) ")
+        print("Sorry, I have no idea what to do with that:( Can you give me something that looks like these two : 'A' or 'B'?")
         continue
     else:
         break
 if option == 'A' :
     print('Great! Time to encrypt a message:)')
+    encryption(key_table)
+
 else :
-    print('Great! Time to decrypt a message:)')   
+    print('Great! Time to decrypt a message:)') 
+    decryption(key_table) 
